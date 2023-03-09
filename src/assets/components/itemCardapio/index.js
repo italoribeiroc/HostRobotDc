d8348edff9c127dbb0bf32579777c8acf501496c
João Hirasawa <joaohirasawaa@gmail.com>
2023-03-09 13:10:26 -0300

    update code for cardapio ru

--- src/assets/components/itemCardapio/index.js
+++ src/assets/components/itemCardapio/index.js
@@ -1,11 +1,9 @@
-import { Avatar, Card, Text } from 'react-native-paper';
-
-const LeftContent = props => <Avatar.Icon {...props} icon="leaf" />;
+import { Card, Text } from 'react-native-paper';
 
 const ItemCardapio = (props) => {
   return (
-    <Card style={{ backgroundColor: '#E8F2FF' }}>
-      <Card.Title title={props.title} subtitle={props.subtitle} left={props.icon}/>
+    <Card className='cardapio-item' style={props.style}>
+      <Card.Title title={props.title} subtitle={props.subtitle} left={props.icon} />
       <Card.Content>
         <Text variant="bodyMedium">{props.content}</Text>
       </Card.Content>
